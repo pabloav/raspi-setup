2d24b8bfa9608e66c98b943344cc1c80d641edd5
pablo <pablo@raspberrypi.(none)>
2012-07-11 19:45:33 +0100

    initial setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Fix /etc/apt/sources.list"
+update-rc.d ssh defaults
+update-rc.d portmap disable
+update-rc.d nfs-common disable
+adduser pablo
+
+
+apt-get update
+apt-get install ruby1.9.1 python-imaging git-core
