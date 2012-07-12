c5b46341de7caad47eafdcd31437738c55e420bf
Pablo <pablo@vanitude.com>
2012-07-12 12:32:46 -0400

    Refactored to use run-parts

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
-echo "Fix /etc/apt/sources.list"
-update-rc.d ssh defaults
-update-rc.d portmap disable
-update-rc.d nfs-common disable
-adduser pablo
-
-
-apt-get update
-apt-get install ruby1.9.1 python-imaging git-core
-
-dpkg-reconfigure tzdata
+chmod a+x setup.d/*
+run-parts setup.d
