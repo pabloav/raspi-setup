46db8a8a0d9ec617610d9327c56d89e5bdacc9f5
Pablo Averbuj <pablo@vanitude.com>
2012-07-19 15:59:24 -0400

    Determine the OS version and use install vs cp

--- setup.sh
+++ setup.sh
@@ -3,5 +3,12 @@
 # To bootstrap
 # wget --no-check-certificate -Oraspi-setup.tgz https://github.com/pabloav/raspi-setup/tarball/master
 
+
+VERSION=squeeze
+if [ grep wheezy /etc/debian_version > /dev/null ]; then
+  VERSION=wheezy
+fi
+export VERSION
+
 chmod a+x setup.d/*
 run-parts setup.d
