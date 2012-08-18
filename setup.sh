3bc03cd22dc5d111ada84880a0d90178ca89f766
Pablo Averbuj <pablo@vanitude.com>
2012-08-18 12:40:54 -0400

    Check it's running as root because I always forget and run it as a user

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@
 # wget --no-check-certificate -Oraspi-setup.tgz https://github.com/pabloav/raspi-setup/tarball/master
 
 
+if [ "$UID" -ne "0" ]; then
+  echo "Please run this as root."
+  exit 1
+fi
+
 VERSION=squeeze
 if grep wheezy /etc/debian_version > /dev/null; then
   VERSION=raspbian
